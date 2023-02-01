a26baf1206d811de6653e2c5ef9723c9c2d27933
Byomokesh Senapati <72374116+metarex21@users.noreply.github.com>
2023-02-01 18:41:44 +0530

    Update and rename q1.py to q1-greatest-common-divisor-of-strings.py

--- q1-greatest-common-divisor-of-strings.py
+++ q1-greatest-common-divisor-of-strings.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1+str2==str2+str1:
